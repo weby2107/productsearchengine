c74d07eff02de2aaa90081ca0172b4f75a992023
Weby <wahabsoomro2107@gmail.com>
2022-12-16 15:59:35 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const search = () => {
+    const searchBox = document.getElementById("search-item").value.toUpperCase();
+    const storeItems = document.getElementById("product-list");
+    const product = document.querySelectorAll(".product");
+    const pname = storeItems.getElementsByTagName("h2");
+
+    for (var i = 0; i < pname.length; i++) {
+        let match = product[i].getElementsByTagName('h2')[0];
+
+        if (match) {
+            let textvalue = match.textContent || match.innerHTML;
+            if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
+                product[i].style.display = "";
+            } else {
+                product[i].style.display = "items not available";
+            }
+
+
+        }
+    }
+};
