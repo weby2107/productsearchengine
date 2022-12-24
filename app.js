80df254f5eb0236226a212b939d2712c7f1fa65b
Weby <wahabsoomro2107@gmail.com>
2022-12-24 16:04:14 +0500

    git commit -m "final"

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const search = () => {
+const search = (event) => {
+    event.preventDefault();
     const searchBox = document.getElementById("search-item").value.toUpperCase();
     const storeItems = document.getElementById("product-list");
     const product = document.querySelectorAll(".product");
     const pname = storeItems.getElementsByTagName("h2");
 
     for (var i = 0; i < pname.length; i++) {
-        let match = product[i].getElementsByTagName('h2')[0];
+        let match = product[i].getElementsByTagName("h2")[0];
 
         if (match) {
             let textvalue = match.textContent || match.innerHTML;
             if (textvalue.toUpperCase().indexOf(searchBox) > -1) {
                 product[i].style.display = "";
             } else {
-                product[i].style.display = "items not available";
+                product[i].style.display = "none";
             }
 
 
